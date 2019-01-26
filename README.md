98f2635e96e3a2ef5f24321f4388c966b649ed8f
Caleb Rascon <caleb.rascon@gmail.com>
2019-01-25 19:35:00 -0600

    Fixed an issue with CMakeList including non-existing nodes. Fixed an issue with rosjack_ref not doing the same overlap-and-add logistics as the beamformers.

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ ROS package that carries out simple 1D beamforming strategies, using JACK as inp
 It includes a custom ROS topic: JackAudio, defined in the package "jack_msgs". It also uses a custom API for communicating between ROS and JACK referenced as "rosjack", which is not to be confused with https://github.com/balkce/rosjack. Although both serve similar purposes, in this repository, rosjack is built as a library for ROS-JACK inter-operability, not as a ROS package.
 
 Included beamformers:
-* das_time: Delay-and-Sum in time domain
 * das: Delay-and-Sum in frequency domain
 * mvdr: Minimum Variance Distortionless Response
 * phase: Phase-based binary masking
@@ -23,6 +22,14 @@ Note for GSC: it uses a dynamic mu that changes depending on the current sample 
 Note for Phase: it process the weight of each frequency bin by considering if the phase is below a threshold; if not, it averages several past magnitudes and multiplies them by a factor to reduce the presence of not-in-phase interferences. To facilitate it's configuration, the phase.launch file includes the values for minimum phase and magnitude factor, and the number of past windows.
 
 Note for audio file: the audio file is a 16-bit WAV file with the sample rate with which the JACK server is configured. If the file path in rosjack_config.yaml is empty, the default path will be used: /home/user/rosjack_write_file.wav.
+
+## Additional utilities included
+
+The following nodes are also included:
+* rosjack_read: example of a node that reads from JACK and outputs to the configured output in rosjack_config.yaml (can be the JackAudio ROS topic, JACK, or both).
+* rosjack_write: example of a node that writes to JACK from theh JackAudio ROS topic. It uses an intermediate buffer to be robust against inconsistencies in timing, but induces some lag between input and output.
+
+In addition, the rosjack_ref node is also included (with its accompanying launch file) which outputs the reference microphone (mic1) applying the overlap-and-add logistics the beamformers use. These logistics insert a lag which makes it difficult to evaluate the beamformers output online. The rosjack_ref node output should synchronize exactly with the output of the beamformers.
 
 ## Dependencies
 Packages that can be installed trough apt official repositories:
--- beamform/src/jack_ref.cpp
+++ beamform/src/jack_ref.cpp
@@ -9,21 +9,11 @@
 #include <vector>
 #include <cmath>
 
-// Include FFTW header
-#include <complex>
-#include <fftw3.h>
-
-// Eigen include
-#include <Eigen/Eigen>
-
 bool READY = false;
 
-std::complex<double> *x_fft, *x_time, *y_fft, *y_time;
-fftw_plan x_forward, y_inverse;
-
 double *hann_win;
-double *freqs;
 unsigned int fft_win;
+unsigned int buf_win;
 
 //reused buffers
 rosjack_data *in_buff;
@@ -45,23 +35,20 @@ int jack_callback (jack_nframes_t nframes, void *arg){
         
         //appending this window to input buffer
         for(j = 0; j < nframes; j++)
-            in_buff[j+(nframes*3)] = in[0][j];
+            in_buff[j+(nframes*5)] = in[0][j];
         
         //applying hann window
-        memcpy(out_buff2, in_buff,sizeof(rosjack_data)*fft_win);
         for(j = 0; j < fft_win; j++){
-            out_buff2[j] *= hann_win[j];
+            out_buff1[j] = in_buff[j]*hann_win[j];
+            out_buff2[j] = in_buff[j+(nframes*2)]*hann_win[j];
         }
 
         //doing overlap and storing in output
         for(j = 0; j < nframes; j++)
-            out[j] = out_buff1[j+(nframes*2)] + out_buff2[j+nframes];
-        
-        //storing filtered output to out_buff1 for overlap purposes
-        memcpy(out_buff1, out_buff2,sizeof(rosjack_data)*fft_win);
+            out[j] = (out_buff1[j+((int)(nframes*2.5))] + out_buff2[j+((int)(nframes*0.5))]);
         
         //shifting input buffer one window
-        for(j = 0;j < fft_win-nframes; j++)
+        for(j = 0;j < buf_win-nframes; j++)
             in_buff[j] = in_buff[j+nframes];
     }else{
         for (i = 0; i < nframes; i++){
@@ -95,14 +82,7 @@ int main (int argc, char *argv[]) {
     
     std::cout << "Pre-allocating space for internal buffers." << std::endl;
     fft_win = rosjack_window_size*4;
-    
-    x_fft = (std::complex<double>*) fftw_malloc(sizeof(std::complex<double>) * fft_win);
-    x_time = (std::complex<double>*) fftw_malloc(sizeof(std::complex<double>) * fft_win);
-    y_fft = (std::complex<double>*) fftw_malloc(sizeof(std::complex<double>) * fft_win);
-    y_time = (std::complex<double>*) fftw_malloc(sizeof(std::complex<double>) * fft_win);
-    
-    x_forward = fftw_plan_dft_1d(fft_win, reinterpret_cast<fftw_complex*>(x_time), reinterpret_cast<fftw_complex*>(x_fft), FFTW_FORWARD, FFTW_MEASURE);
-    y_inverse = fftw_plan_dft_1d(fft_win, reinterpret_cast<fftw_complex*>(y_fft), reinterpret_cast<fftw_complex*>(y_time), FFTW_BACKWARD, FFTW_MEASURE);
+    buf_win = rosjack_window_size*6;
     
     hann_win = (double *) malloc(sizeof(double) * fft_win);
     for (i = 0; i < fft_win; i++){
@@ -111,7 +91,7 @@ int main (int argc, char *argv[]) {
     out_buff1 = (rosjack_data *) malloc (sizeof(rosjack_data)*fft_win);
     out_buff2 = (rosjack_data *) malloc (sizeof(rosjack_data)*fft_win);
     
-    in_buff = (rosjack_data *) malloc (sizeof(rosjack_data)*fft_win);
+    in_buff = (rosjack_data *) malloc (sizeof(rosjack_data)*buf_win);
     
     READY = true;
     
