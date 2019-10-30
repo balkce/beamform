b1b0d3655a1a8070e897dfa4bb7f56a04b233ba8
Caleb Rascon <caleb.rascon@gmail.com>
2019-10-29 20:19:37 -0600

    Threaded the other protocol node.

--- beamform/scripts/speakeridrest.py
+++ beamform/scripts/speakeridrest.py
@@ -3,19 +3,40 @@
 import rospy
 from std_msgs.msg import String
 from jack_msgs.msg import JackAudio
+import threading
+import time
+
+startflag = False
+
+this_win = []
 
 def callback(data):
-    global pub
+    global this_win
+    global startflag
     
     # read from jackaudio
     this_win = list(data.data)
-    rospy.loginfo(rospy.get_caller_id() + "I heard %f : length %d", this_win[0], len(this_win))
+    #rospy.loginfo(rospy.get_caller_id() + "I heard %f : length %d", this_win[0], len(this_win))
+    
+    if not startflag:
+        print (str(startflag))
+        startflag = True
+
+def thread_function(name):
+    global pub
+    global this_win
+    global startflag
     
     #publish to speakerid topic
-    hello_str = "hello world %s" % rospy.get_time()
-    rospy.loginfo(hello_str)
-    pub.publish(hello_str)
+    while not startflag:
+        time.sleep(1)
     
+    rate = rospy.Rate(10) # 10hz
+    while True:
+        hello_str = "hello world %s" % this_win[0]
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
 
 rospy.loginfo("Starting speakeridrest node...")
 
@@ -24,6 +45,10 @@ rospy.init_node('speakeridrest', anonymous=True)
 try:
     pub = rospy.Publisher('speakerid', String, queue_size=10)
     rospy.Subscriber("jackaudio", JackAudio, callback)
+    
+    x = threading.Thread(target=thread_function, args=(1,))
+    x.start()
+    
 except rospy.ROSInterruptException:
     pass
 
